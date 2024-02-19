c346a6ec0e18cb9dc3957dfc37324512b7cef49b
kachnna <137805850+kachnna@users.noreply.github.com>
2024-02-19 20:16:01 +0100

    View

--- Moduł II/zadanie domowe 4/main.py
+++ Moduł II/zadanie domowe 4/main.py
@@ -4,13 +4,12 @@ import socket
 import mimetypes
 import json
 import pathlib
-from urllib.parse import parse_qs, urlparse
+import urllib.parse
 from datetime import datetime
 import threading
 import os
-from http import HTTPStatus
 
-HOST = 'localhost'
+HOST = '127.0.0.1'
 HTTP_PORT = 3000
 SOCKET_PORT = 5000
 DATA_FILE_PATH = 'storage/data.json'
@@ -20,29 +19,59 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
 
 
+def _write_to_json(message_data):
+    if not os.path.exists('storage'):
+        os.makedirs('storage')
+        print("Folder 'storage' was created")
+
+    timestamp = message_data['timestamp']
+    data_to_write = {timestamp: {
+        'username': message_data['username'], 'message': message_data['message']}}
+
+    with open(DATA_FILE_PATH, 'a') as file:
+        json.dump(data_to_write, file, indent=2)
+        file.write('\n')
+
+
+def _process_form(data):
+    messages = []
+    if 'username' in data and 'message' in data:
+        username = data['username'][0]
+        message = data['message'][0]
+        timestamp = datetime.now().isoformat()
+
+        message_data = {
+            'username': username,
+            'message': message,
+            'timestamp': timestamp
+        }
+        messages.append(message_data)
+        _write_to_json(message_data)
+
+
 class HTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        pr_url = urlparse(self.path)
+        pr_url = urllib.parse.urlparse(self.path)
 
         if pr_url.path == '/':
-            self.send_html_file("./index.html")
-        elif pr_url.path == '/message.html':
-            self.send_html_file("./message.html")
+            self.send_html_file("index.html")
+        elif pr_url.path == '/message':
+            self.send_html_file("message.html")
         else:
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
             else:
-                self.send_html_file("./error.html", HTTPStatus.NOT_FOUND.value)
+                self.send_html_file("error.html", 404)
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
-        parsed_data = parse_qs(post_data)
+        print(post_data)
 
-        self._process_form(parsed_data)
+        _process_form(urllib.parse.parse_qs(post_data))
 
-        self.send_response(303)
-        self.send_header('Location', 'message.html')
+        self.send_response(302)
+        self.send_header('Location', '/')
         self.end_headers()
 
     def send_static(self):
@@ -56,36 +85,12 @@ class HTTPHandler(BaseHTTPRequestHandler):
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
 
-    def send_html_file(self, filename, status=HTTPStatus.OK.value):
+    def send_html_file(self, filename, status=202):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
-
-    def _process_form(self, data):
-        messages = []
-        if 'username' in data and 'message' in data:
-            username = data['username'][0]
-            message = data['message'][0]
-            timestamp = datetime.now().isoformat()
-
-            message_data = {
-                'username': username,
-                'message': message,
-                'timestamp': timestamp
-            }
-            messages.append(message_data)
-            self._write_to_json(message_data)
-
-    def _write_to_json(self, message_data):
-        if not os.path.exists('storage'):
-            os.makedirs('storage')
-            print("Folder 'storage' was created")
-
-        with open(DATA_FILE_PATH, 'a') as file:
-            json.dump(message_data, file)
-            file.write('\n')
 
 
 class SocketServerThread(threading.Thread):
@@ -104,7 +109,6 @@ class SocketServerThread(threading.Thread):
 
 
 def main():
-
     http_server = ThreadedHTTPServer((HOST, HTTP_PORT), HTTPHandler)
     http_thread = threading.Thread(target=http_server.serve_forever)
     http_thread.daemon = True
@@ -114,6 +118,13 @@ def main():
     socket_server_thread = SocketServerThread(HOST, SOCKET_PORT)
     socket_server_thread.daemon = True
     socket_server_thread.start()
+
+    print("Servers started successfully!")
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
 
 
 if __name__ == "__main__":
