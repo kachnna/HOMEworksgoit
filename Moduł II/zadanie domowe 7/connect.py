dc8d9e4983aaeaf2e63b2df33d9d3f2ded306fb7
kachnna <137805850+kachnna@users.noreply.github.com>
2024-02-29 22:23:01 +0100

    del

--- Moduł II/zadanie domowe 7/connect.py
+++ Moduł II/zadanie domowe 7/connect.py
@@ -4,6 +4,5 @@ from models import Base
 
 # docker run --name school -p 5432:5432 -e POSTGRES_PASSWORD=1234 -d postgres
 engine = create_engine('sqlite:///school.db')
-Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
