54d17873234c1bae3532d221e89b488d0f5357e3
kachnna <137805850+kachnna@users.noreply.github.com>
2024-04-11 00:05:16 +0200

    wokr

--- Moduł II/zadanie domowe 13/Task1/main.py
+++ Moduł II/zadanie domowe 13/Task1/main.py
@@ -74,7 +74,3 @@ async def startup():
 @app.get("/")
 def read_root():
     return {"message": "Hello in my application!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- Moduł II/zadanie domowe 13/Task1/src/repository/users.py
+++ Moduł II/zadanie domowe 13/Task1/src/repository/users.py
@@ -4,8 +4,10 @@ from sqlalchemy.orm import Session
 from src.database.models import User
 from src.schemas import UserIn, UserOut
 
+
 async def get_user_by_username(username: str, db: Session) -> UserOut:
     return db.query(User).filter(User.username == username).first()
+
 
 async def create_user(body: UserIn, db: Session) -> UserOut:
     avatar = None
@@ -20,17 +22,21 @@ async def create_user(body: UserIn, db: Session) -> UserOut:
     db.refresh(new_user)
     return new_user
 
+
 async def update_token(user: User, token: str | None, db: Session) -> None:
     user.refresh_token = token
     db.commit()
 
-# async def get_user_by_email(email: str, db: Session) -> UserOut:
-#     return db.query(User).filter(User.email == email).first()
+
+async def get_user_by_email(email: str, db: Session) -> UserOut:
+    return db.query(User).filter(User.email == email).first()
+
 
 async def confirmed_email(username: str, db: Session) -> None:
     user = await get_user_by_username(username, db)
     user.confirmed = True
     db.commit()
+
 
 async def update_avatar(username, url: str, db: Session) -> UserOut:
     user = await get_user_by_username(username, db)
