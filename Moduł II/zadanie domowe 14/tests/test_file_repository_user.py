57465b4e3fc07f253dbbe3354e0428c219ebd67b
kachnna <137805850+kachnna@users.noreply.github.com>
2024-04-15 21:11:42 +0200

    e

--- Moduł II/zadanie domowe 14/tests/test_file_repository_user.py
+++ Moduł II/zadanie domowe 14/tests/test_file_repository_user.py
@@ -27,7 +27,7 @@ class TestUsers(unittest.IsolatedAsyncioTestCase):
     async def test_create_user(self):
         body = UserIn(
             username="kachnna",
-            email="",
+            email="sara486@.pl",
             password=""
         )
         result = await create_user(body=body, db=self.session)
